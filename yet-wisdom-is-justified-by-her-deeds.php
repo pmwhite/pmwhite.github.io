516196f459fd359b75258b8f78b1940e22fd6f3f
Philip White <code@trailingwhite.space>
2025-12-02 23:16:23 -0500

    Add new post

---
+++ yet-wisdom-is-justified-by-her-deeds.php
@@ -0,0 +1,138 @@
+<?php
+include "common.php";
+essay_begin($yet_wisdom_is_justified_by_her_deeds_page);
+?>
+
+<p>Sometimes people begin, either in debate or in their own thinking, first by
+settling beliefs and then by discovering the behavior produced by such
+beliefs. In my view, beliefs and behavior are not so easily disentangled,
+especially when it comes to communicating our beliefs to others; that is, it is
+often difficult to truly understand what someone believes until they explain
+how it affects their behavior.</p>
+
+<p>Take, for instance, the (in)famous "sandwich debate". Some take an extreme
+view that anything between two other things counts as a sandwich. In this view,
+a board of would between two slabs of rocks would count as a sandwich; even
+three molecules in a row count as a sandwich, since one of them is between the
+other two. Some who oppose this view reject these examples because they are not
+food; a sandwich is for eating, they say. But even amongst this view there is
+disagreement on how to address hot dogs and tacos.</p>
+
+<p>Which view is right? Of course, it depends on the context in which the word
+"sandwich" was used. If someone says, "go make me a sandwich," they'll be
+disappointed if you bring brack two rocks with wood in between. But if they
+instead say, "sandwich that wood between these rocks," it wouldn't be
+appropriate to complain that such a setup is not technically a sandwich because
+you can't eat it. The point is that context matters, so the sandwich debate is
+about <em>semantics</em> instead of <em>truth</em>. Neither view is "right";
+rather, they are more or less useful in different circumstances. Those engaged
+in the sandwich debate do not actually disagree on anything; rather, they are
+merely speaking different languages (or, if you like, different dialects of the
+same language).</p>
+
+<p>The sandwich debate is a silly one, but there are a lot more serious matters
+that have a similar pattern. Sometimes friends and spouses get into seriously
+tense arguments about a topic on which they agree about everything but the way
+they explain their position. Theological debates, too, often end up
+missing the crux of the matter due to a mere disagreement in terms (more on
+this later).</p>
+
+<p>In my view, truth is fundamentally related to behavior. If a tree falls in
+the forest and no one is around, does it make a sound? I choose not to engage
+with the question, since it makes no difference to me. I'm not merely
+uncurious; it is additionally my belief that something is not true unless it
+affects an observer in some way. Not that a proposition is false if it has no
+effect on anyone - it is just <em>moot</em>.</p>
+
+<p>The view I have just expressed may sound post-modern and relativistic, and
+in some ways it is, but it does not reject objective reality. If you say,
+"quiche is the yummiest breakfast," I'll happily reply, "That's true for you, but
+not for me." What I would mean by that is that <em>you</em> should adjust your
+behavior based on that statement, but I should not (in fact, I very much
+dislike quiche). In contrast, if you say, "I bet jumping off a cliff is a lot
+of fun," I'd instead say, "you're in for a rude awakening." Truth is a servant
+of reality.</p>
+
+<p>Interestingly, reality is not a hard master. Often there are multiple models
+of reality that turn out to be suitable. The foundation of physics has gone
+through several iterations, including Newton's Laws, Einstein's Relativity, 
+Quantum physics, and a wealth of other outlandish new ideas. Each model has its
+strengths and weaknesses, but they're all true in the sense that they are
+<em>useful</em>. Truth has some flexibility in everyday life, too. What if I,
+too, decided to model my behavior off the assumption that quiche was the
+yummiest breakfast? I would end up eating a lot more quiche, which would result
+in more unpleasant breakfasts, but also more healthy ones, since I would be
+eating more eggs and fewer donuts. Would it then be true that quiche is yummy
+to me? In most cases I would say no, but if I'm hanging out with my health nut
+friends, I might say something like, "man, I sure do feel good after eating
+quiche", which isn't too far off saying that it's yummy.</p>
+
+<p>I hope it is already evident where I stand on this, but I want to be clear
+that I believe in absolute truth, and the importance of honesty. When speaking
+to others, we're in a shared context in which we generally know how they will
+understand something we say; thus, it is dishonest to say something we know to
+be false and then say, "well, I was not technically lying, since it's true in
+this other context." To the contrary, in my view the honest thing to do is to
+take into account the current context at all times. If anything, I think my
+view of truth values honesty more high and judges it more strictly than simpler
+views on truth.</p>
+
+<p><b>In short, what I'm advocating for is that if you are going to debate a
+strong philosophical opinion, be prepared with a reason why your opinion
+matters. In other words, what behavior change does it call for?</b></p>
+
+<p>One of the debates that motivated this essay is the classic question of how
+God's sovereignty interacts with man's free will. Calvinists tend to emphasize
+God's sovereignty more than free will, and armenians tend to do the opposite;
+compatibilism is the view that both coexist in a mystery that cannot be
+fully understood by our limited capacities.</p>
+
+<p>I've participated in this sort of debate countless times, usually taking the
+compatibilist view, with a stronger emphasis on calvinism for whatever reason.
+Every time, I come away disappointed with the result; I've never succeeded in
+changing anyone's mind, and I always end up feeling like we haven't actually
+gotten to the heart of the matter. The discussion often gets stuck on more
+abstract points like whether God is 100% sovereign, whether sovereignty and
+free will contradict, if sovereignty turns people into robots, and whether
+God's work stops at regeneration or goes all the way to saving faith.</p>
+
+<p>What <em>is</em> the heart of the matter? Again, the point of this essay is
+that a philosophical position is fundamentally related to the behavior it
+prescribes. The Bible tells us many things about God, man, and how they should
+relate; some are more abstract and some are more concrete, but they all are
+meant to change us. As I've tried to argue here, truth is never about filling
+out brain with fun facts; it's about equipping for good works.</p>
+
+<p>I don't plan to avoid debates about election in the future, but I do plan to
+focus far more heavily on behavioral questions, and I expect to find a lot of
+common ground, and perhaps to discover the <em>actual</em> disagreements that
+exist. My experience is that many calvinists actually do believe in evangelism,
+and many armenians believe that God's plans cannot be thwarted and God will
+sustain the believer to the end. But there are lot of nuanced behavior
+questions that perhaps they would have differing views on. How obnoxious should
+evangelism be? Should we be doing alter calls all the time, or take a more
+reserved approach? If someone leaves Christianity, should we attempt to win
+them back? I have thoughts on each of these questions, of course, but this is
+beyond the scope of this essay.</p>
+
+<p>As a final note, I'd like to point out a couple places where the Bible seems
+to take the same view of truth that I do. First, in Matthew 11:19 (ESV), Jesus
+says, "Yet wisdom is justified by her deeds." The context is that no matter
+what Jesus did, the Pharisees would always take issue; they could never be
+pleased. If he ate food they would call him a drunkard, but if he fasted they
+would say he has a demon. But Jesus responds by noting that wisdom is proven
+right by the fruit that it produces; they could call Jesus a drunkard all they
+want, but he made the blind see, the lame walk, the deaf hear, the sick were
+healed, and the lost were saved. It's not that the wisdom itself is
+unimportant, but the clearest demonstration what it is really all about is in
+the deeds that it produces.</p>
+
+<p>Second, consider 1 Corinthians 8, in which Paul addresses the matter of
+eating food offered to idols. He say, "knowledge puffs up, but love builds up."
+(ESV), by which he means that the brain knowledge behind our actions is often
+less important than the heart. In his example, even if we have the knowledge
+that its okay to eat food offered to idols, it's still important to be
+considerate of those whose conscience keeps them from doing so. So our goal in
+seeking truth is not to merely find the correct facts and know the right
+things, but also to build up our own character and the character of those
+around, so that we can honor the Lord with what we <em>do.</em></p>
