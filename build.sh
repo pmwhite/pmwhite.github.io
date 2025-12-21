d13d33734121bc7da893f52004f4e4a2ea2eb8fd
Philip White <code@trailingwhite.space>
2025-12-21 12:48:59 -0500

    Add bfm2000 WIP

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ php -f modularity.php > modularity.html
 php -f cycling-log-2025.php > cycling-log-2025.html
 php -f parts-of-a-pl-implementation.php > parts-of-a-pl-implementation.html
 php -f yet-wisdom-is-justified-by-her-deeds.php > yet-wisdom-is-justified-by-her-deeds.html
+php -f bfm2000.php > bfm2000.html
